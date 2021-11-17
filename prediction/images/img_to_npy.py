ec06bd4c3e409472c446cecc327e71e8251333dc
Stefan Nica <snica@suse.com>
2021-11-17 10:06:09 +0100

    Add CIFAR10 images for prediction and CNN prediction scripts
    Add a set of images that can be used to run inference requests
    against a CIFAR10 model.
    
    Add the following scripts imported from 3rd party sources
    and slightly modified to be more flexible to be used with any
    CNN models served under an API that is compatible with the
    TensorFlow serving REST API:
    
    - img_to_npy.py (imported from [1]) - this script is used
    to convert a set of raw images into a numpy dataset. The numpy
    dataset format is used by other scripts as input to perform batch
    prediction request or other similar functions
    
    - generate_rest_payload.py - use this script to generate a JSON
    request payload that can be used for TensorFlow REST prediction
    requests (e.g. with curl). It takes in a numpy dataset and outputs
    a JSON file with a CNN prediction request payload
    
    - rest_binary_client.py (imported from [2]) - an OpenVINO Model
    Server client utility that can be used to send REST binary [3] prediction
    requests to an OpenVINO Model Server starting from an input numpy dataset.
    The original script has been slightly modified to allow it to work
    with any model, not just CIFAR-100.
    
    - rest_serving_client.py (imported from [4]) - an OpenVINO Model
    Server client utility that can be used to send REST prediction
    requests to a TensorFlow serving API starting from an input numpy dataset.
    The original script has been slightly modified to allow it to work
    with any model, not just CIFAR-100. Note that this script works with
    any inference service as long as its API is compatible with the
    TensorFlow REST serving API. It should also work with any CNN model.
    
    [1] https://github.com/foolmarks/images_to_npy/blob/master/img_to_npy.py
    [2] https://github.com/openvinotoolkit/model_server/blob/main/example_client/rest_binary_client.py
    [3] https://github.com/openvinotoolkit/model_server/blob/main/docs/binary_input.md
    [4] https://github.com/openvinotoolkit/model_server/blob/main/example_client/rest_serving_client.py

---
+++ prediction/images/img_to_npy.py
@@ -0,0 +1,155 @@
+
+import os, sys
+import numpy as np
+import cv2
+import argparse
+
+
+DIVIDER = '-------------------------------------'
+
+
+def center_crop(image,out_height,out_width):
+  input_height, input_width = image.shape[:2]
+  offset_height = (input_height - out_height) // 2
+  offset_width = (input_width - out_width) // 2
+  image = image[offset_height:offset_height+out_height, offset_width:offset_width+out_width,:]
+  return image
+
+
+def resize_maintain_aspect(image,target_h,target_w):
+  input_height, input_width = image.shape[:2]
+  if input_height > input_width:
+    new_width = target_w
+    new_height = int(input_height*(target_w/input_width))
+  else:
+    new_height = target_h
+    new_width = int(input_width*(target_h/input_height))
+
+  image = cv2.resize(image,(new_width,new_height),interpolation=cv2.INTER_CUBIC)
+  return image
+
+
+
+def images_to_npy(image_dir,label_file,classes,input_height,input_width,input_chans,resize,normalize,one_hot,compress,output_file,output_label_file):
+
+  # open & read text file that lists all images and their labels
+  f = open(label_file, 'r') 
+  listImages = f.readlines()
+  f.close()
+
+  # make image placeholder array - float32 if resizing and/or normalizing
+  if normalize:
+    x = np.ndarray(shape=(len(listImages),input_height,input_width,input_chans), dtype=np.float32, order='C')
+  else:
+    x = np.ndarray(shape=(len(listImages),input_height,input_width,input_chans), dtype=np.uint8, order='C')
+
+  # make labels placeholder array
+  if (one_hot):
+    y = np.ndarray(shape=(len(listImages),classes), dtype=np.uint8, order='C')
+  elif(classes<=256):
+    y = np.ndarray(shape=(len(listImages)), dtype=np.uint8, order='C')
+  elif(classes<=65536):
+    y = np.ndarray(shape=(len(listImages)), dtype=np.uint16, order='C')
+  else:
+    y = np.ndarray(shape=(len(listImages)), dtype=np.uint32, order='C')
+
+
+
+  for i in range(len(listImages)):
+
+    image_name,label = listImages[i].split()
+
+    # open image to numpy array and switch to RGB from BGR
+    img = cv2.imread(os.path.join(image_dir,image_name))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # optionally resize & center crop
+    if resize:
+      img = resize_maintain_aspect(img,input_height,input_width)
+      img = center_crop(img,input_height,input_width)
+    
+    # optionally normalize then write into placeholder array
+    if normalize:
+      x[i] = (img/255.0).astype(np.float32)
+    else:
+      x[i] = img
+
+    # optionally 1-hot encode the label, then write into placeholder array
+    if (one_hot):
+      label_1hot = np.zeros(classes,dtype=np.uint32,order='C')
+      np.put(label_1hot,int(label),1)
+      y[i] = label_1hot
+    else:
+      y[i] = int(label)
+
+
+  # report data types used
+  print(' x shape:',x.shape)
+  print(' x data type:',x[0].dtype)
+  print(' y shape:',y.shape)
+  print(' y data type:',y[0].dtype)
+
+
+  # write output file(s)
+  save=np.save
+  if (compress):
+    save=np.save_compressed
+  
+  if (output_label_file):
+    save(output_file, x)
+    save(output_label_file, y)
+    print(' Data saved to',output_file)
+    print(' Labels saved to',output_label_file)
+  else:
+    save(output_file, x=x, y=y)
+    print(' Saved to',output_file)
+
+  return  
+
+
+
+def run_main():
+    
+  # construct the argument parser and parse the arguments
+  ap = argparse.ArgumentParser()
+  ap.add_argument('-id','--image_dir',           type=str,   default='image_dir', help='Full path of folder containing images.')
+  ap.add_argument('-l', '--label_file',          type=str,   default='val.txt',   help='Full path of label file.')
+  ap.add_argument('-c', '--classes',             type=int,   default=1000,        help='Number of classes.')
+  ap.add_argument('-ih','--input_height',        type=int,   default=224,         help='Input image height in pixels.')
+  ap.add_argument('-iw','--input_width',         type=int,   default=224,         help='Input image width in pixels.')
+  ap.add_argument('-ic','--input_chans',         type=int,   default=3,           help='Input image channels.')
+  ap.add_argument('-r', '--resize',              action='store_true', help='Resize and center crop images if set. Default is no resize.')
+  ap.add_argument('-n', '--normalize',           action='store_true', help='Normalize pixels to range 0,1 if set. Default is no normalization.')
+  ap.add_argument('-oh','--one_hot',             action='store_true',  help='One-hot encode the labels if set. Default is no encoding.')
+  ap.add_argument('-cp','--compress',            action='store_true', help='Compress the output file if set, otherwise no compression. Default is no compression.')
+  ap.add_argument('-o', '--output_file',         type=str,   default='dataset.npz', help='Full path of output file.')
+  ap.add_argument('-ol','--output_label_file',   type=str,   default='', help='Full path of output labels file.')
+  args = ap.parse_args()
+
+  print('\n'+DIVIDER)
+  print(sys.version)
+  print(DIVIDER)
+  print(' Command line options:')
+  print('--image_dir           : ',args.image_dir    )
+  print('--label_file          : ',args.label_file   )
+  print('--classes             : ',args.classes      )
+  print('--input_height        : ',args.input_height )
+  print('--input_width         : ',args.input_width  )
+  print('--input_chans         : ',args.input_chans  )
+  print('--resize              : ',args.resize       )
+  print('--normalize           : ',args.normalize    )
+  print('--one_hot             : ',args.one_hot      )
+  print('--compress            : ',args.compress     )
+  print('--output_file         : ',args.output_file  )
+  print('--output_label_file   : ',args.output_label_file  )
+  print(DIVIDER)
+  
+
+  images_to_npy(args.image_dir, args.label_file, args.classes, args.input_height, \
+  args.input_width, args.input_chans, args.resize, args.normalize, args.one_hot, \
+  args.compress, args.output_file, args.output_label_file)
+
+
+if __name__ == '__main__':
+  run_main()
+
